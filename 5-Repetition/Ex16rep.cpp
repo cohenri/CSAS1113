8c5fc2043f223a5ad0121b911269854824dadd0f
cohenri <59699557+cohenri@users.noreply.github.com>
2022-03-28 12:18:10 -0400

    Add files via upload

---
+++ 5-Repetition/Ex16rep.cpp
@@ -0,0 +1,17 @@
+// Ex16rep.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+
+    // while loop from 1 to 10
+    while (i <= 10) {
+        cout << i << " ";
+        ++i;
+    }
+
+    return 0;
+}
